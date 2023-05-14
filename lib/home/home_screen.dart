1f485a3834c68b4d6ee974c4673fc9b97cfd0a4f
minju kim <62362753+alswn2348@users.noreply.github.com>
2023-05-14 19:55:42 +0900

    design : 메뉴 화면 디자인
    drawer 메뉴 화면 디자인 추가

--- lib/home/home_screen.dart
+++ lib/home/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:healfooyou_flutter/constants/sizes.dart';
 
 final tabs = ["다이어트", "벌크업", "건강"];
 
@@ -37,20 +38,65 @@ class HomeScreen extends StatelessWidget {
           ],
         ),
         drawer: Drawer(
-          child: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                const Text('This is the Drawer'),
-                ElevatedButton(
-                  onPressed: closeDrawer,
-                  child: const Text('Close Drawer'),
-                ),
-              ],
+          child: SafeArea(
+            child: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Container(
+                    alignment: Alignment.center,
+                    width: double.infinity,
+                    decoration: const BoxDecoration(color: Colors.red),
+                    padding: const EdgeInsets.all(Sizes.size10),
+                    child: const Text(
+                      '메뉴',
+                      style: TextStyle(
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                  const MenuButton(title: "도움말"),
+                  const MenuButton(title: "공지사항"),
+                  const MenuButton(title: "분석요청"),
+                  const MenuButton(title: "문의하기"),
+                  const MenuButton(title: "설정"),
+                ],
+              ),
             ),
           ),
         ),
         drawerEnableOpenDragGesture: false,
+      ),
+    );
+  }
+}
+
+class MenuButton extends StatelessWidget {
+  const MenuButton({
+    super.key,
+    required this.title,
+  });
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      alignment: Alignment.center,
+      width: double.infinity,
+      decoration: BoxDecoration(
+        border: Border.symmetric(
+          horizontal: BorderSide(
+            color: Colors.grey.shade400,
+            width: 0.5,
+          ),
+        ),
+      ),
+      padding: const EdgeInsets.all(Sizes.size10),
+      child: Text(
+        title,
+        style: const TextStyle(
+          color: Colors.black,
+        ),
       ),
     );
   }
